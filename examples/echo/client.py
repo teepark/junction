c7e631e9a554c0aeb7bb45e2a7decca16562d836
Travis J Parker <travis.parker@gmail.com>
2010-11-09 18:45:22 -0800

    add a relay acting both as server and client, and let client.py optionally connect there instead

--- examples/echo/client.py
+++ examples/echo/client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # vim: fileencoding=utf8:et:sta:ai:sw=4:ts=4:sts=4
 
+import sys
 import traceback
 
 import greenhouse
@@ -9,22 +10,27 @@ from junction import Node
 HOST = "127.0.0.1"
 PORT = 12345
 
+RELAY_ADDR = (HOST, 9100)
 SERVICE_ADDR = (HOST, 9000)
 
 SERVICE = 1
+RELAY_SERVICE = 2
 
 
 greenhouse.add_exception_handler(traceback.print_exception)
 
 
 def main():
-    node = Node((HOST, PORT), [SERVICE_ADDR])
+    service = RELAY_SERVICE if '-r' in sys.argv else SERVICE
+    peer_addr = RELAY_ADDR if '-r' in sys.argv else SERVICE_ADDR
+
+    node = Node((HOST, PORT), [peer_addr])
     node.start()
     node.wait_on_connections()
 
-    print node.rpc(SERVICE, "echo", 0, ('one',), {})
+    print node.rpc(service, "echo", 0, ('one',), {})
 
-    counters = map(lambda msg: node.send_rpc(SERVICE, "echo", 0, (msg,), {}),
+    counters = map(lambda msg: node.send_rpc(service, "echo", 0, (msg,), {}),
             ('two', 'three', 'four', 'five'))
     print '\n'.join(map(repr, map(node.wait_rpc, counters)))
 
---
+++ examples/echo/relayer.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# vim: fileencoding=utf8:et:sta:ai:sw=4:ts=4:sts=4
+
+import traceback
+
+import greenhouse
+from junction import Node
+
+HOST = "127.0.0.1"
+PORT = 9100
+
+SERVICE_HOST = HOST
+SERVICE_PORT = 9000
+
+BDHOST = HOST
+BDPORT = 9101
+
+FRONT_SERVICE = 2
+BACK_SERVICE = 1
+
+greenhouse.add_exception_handler(traceback.print_exception)
+
+node = Node((HOST, PORT), [(SERVICE_HOST, SERVICE_PORT)])
+
+
+def handler(x):
+    return ["forwarded %s" % r for r in
+            node.rpc(BACK_SERVICE, "echo", 0, (x,), {})]
+
+
+def main():
+    node.accept_rpc(FRONT_SERVICE, "echo", 0, 0, handler)
+
+    node.start()
+    node.wait_on_connections()
+
+    greenhouse.schedule(greenhouse.run_backdoor,
+            args=((BDHOST, BDPORT), {'node': node}))
+
+    try:
+        greenhouse.Event().wait()
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
