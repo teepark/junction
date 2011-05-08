6adde949e3490387845e5b50dc9d9413103ea60d
Travis J Parker <travis.parker@gmail.com>
2011-05-08 09:38:52 -0700

    exception msg change
    Change-Id: Ib06b96798f2224b694316be3979130bf348d34f1

--- junction/errors.py
+++ junction/errors.py
@@ -31,7 +31,7 @@ HANDLED_ERROR_TYPES = {}
 class _MetaHandledError(type):
     def __init__(cls, *args, **kwargs):
         if cls.code in HANDLED_ERROR_TYPES:
-            raise Exception("HandledError subclasses need unique 'code's")
+            raise Exception("HandledError subclasses need unique codes")
 
         HANDLED_ERROR_TYPES[cls.code] = cls
 
