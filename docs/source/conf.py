8d5e1ca5022de80ae6ed1364acaabc1ef2c379f5
Travis Parker <travis.parker@gmail.com>
2011-05-31 17:47:29 -0700

    add an intro/tutorial for future objects to the docs
    Change-Id: Ic4b5797a26e1d0e7fd85071a9bbff9b3ce2c433c

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,8 @@ sys.path.append(os.sep.join(os.path.abspath(__file__).split(os.sep)[:-3]))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+extensions = ['sphinx.ext.autodoc']
+#extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
